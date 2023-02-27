5963d6657d11ba993285c92016006a794c710f39
gaurabwebdev <gaurabbose40@gmail.com>
2023-02-27 11:18:44 +0600

    data load test completed]

--- js/app.js
+++ js/app.js
@@ -6,7 +6,27 @@ const loadMobiles = async() =>{
 }
 
 const displayMobiles = (mobiles) => {
-    console.log(mobiles)
+    const mobilesSection = document.getElementById('mobiles-container');
+    mobiles.forEach((mobile) => {
+        // console.log(mobile);
+        const {brand, image, phone_name, slug} = mobile;
+        const mobileDiv = document.createElement('div');
+        mobileDiv.classList.add('col');
+        mobileDiv.innerHTML = `
+        <div class="card">
+            <img class="m-5 product-image" src="${image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">
+                ${phone_name}
+                </h5>
+                <h5 class="card-title">
+                ${brand}
+                </h5>
+            </div>
+        </div>
+        `;
+        mobilesSection.appendChild(mobileDiv);
+    })
 }
 
 loadMobiles();
