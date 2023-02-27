8a1aacd3e709e6848a4b81ce0c779c3d3caaae0a
gaurabwebdev <gaurabbose40@gmail.com>
2023-02-27 11:02:54 +0600

    header section done

--- js/app.js
+++ js/app.js
@@ -2,10 +2,12 @@ const loadMobiles = async() =>{
     const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data.data);
+    displayMobiles(data.data);
 }
 
-
+const displayMobiles = (mobiles) => {
+    console.log(mobiles)
+}
 
 loadMobiles();
 
