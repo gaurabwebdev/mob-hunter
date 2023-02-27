1bb6c71538f9de7f5553a25b9a3c507401c51188
gaurabwebdev <gaurabbose40@gmail.com>
2023-02-27 10:59:32 +0600

    data of mobiles loaded from api

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-console.log('app connected')
+const loadMobiles = async() =>{
+    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.data);
+}
+
+
+
+loadMobiles();
+
+
+
+
+
+
+
+
+
